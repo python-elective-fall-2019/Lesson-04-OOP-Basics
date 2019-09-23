adb9ee97190fd68f0610d70e6bc5b7c611c80672
clbokea <clbo@kea.dk>
2019-09-23 10:27:35 +0200

    bank exercise

---
+++ exercises/bank_account.py
@@ -0,0 +1,15 @@
+""" Bank Exercise 
+
+Create a 
+
+* Bank class
+* Account Class
+* Customer class
+
+The bank class should be able to hold many account.
+The Account class should have relevant details.
+The Customer class Should also have relevant details.
+
+Stick to what we covered so far.
+
+"""
