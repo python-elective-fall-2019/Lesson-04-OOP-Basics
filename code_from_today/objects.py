c08b4fd1d97ca2901827798c1af056129b42e7f0
clbokea <clbo@kea.dk>
2019-09-23 10:19:16 +0200

    class

---
+++ code_from_today/objects.py
@@ -0,0 +1,21 @@
+
+class Person:
+   
+    type = 'Mammel' 
+
+    # Constructor
+    def __init__(self, name, type):
+        self.name = name  #Instance variable 
+        Person.type = type
+
+
+
+
+
+
+
+
+
+
+
+
